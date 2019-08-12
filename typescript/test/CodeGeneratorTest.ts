e0fd17031a8df6e067970f1f7b1cf262d6e77d7a
xiaozhenliugg <xiaozhenliu@google.com>
2019-08-12 10:30:21 -0700

    working init test

--- typescript/test/CodeGeneratorTest.ts
+++ typescript/test/CodeGeneratorTest.ts
@@ -1,21 +1,19 @@
 const execa = require('execa');
 const path = require('path');
-const GENERATED_CLIENT_LIB_DIR = '--typescript_gapic_out=' + path.join(__dirname, '..', '..', '..') + '/showcase/tmp_xz1 ';
-const GOOGLE_GAX_PROTOS_DIR = '-I/usr/local/lib/node_modules/google-gax/protos ';
-const CLIENT_LIB_DIR = '-I/Users/xiaozhenliu/showcase ';
+const GENERATED_CLIENT_LIB_DIR = '--typescript_gapic_out=' + path.join(__dirname, '..', '..', '..') + '/showcase/tmp_xz1';
+const GENERATED_CLIENT_FILE = path.join(__dirname, '..', '..', '..') + '/showcase/tmp_xz1/src/v1beta1/echo_client.ts'
+const GOOGLE_GAX_PROTOS_DIR = '-I/usr/local/lib/node_modules/google-gax/protos';
+const CLIENT_LIB_DIR = '-I/Users/xiaozhenliu/showcase';
 const PROTO_DIR = 'google/showcase/v1beta1/echo.proto';
-const testDestination = path.join(__dirname, '..', '..');
+const CLIENT_LIBRARY_BASELINE = __dirname + '/testdata/src/v1beta1/echo_client_baseline.ts';
 
 
 describe('CodeGeneratorTest', () => {
   describe('Generate showcase library', () => {
     it('should return same good output with baseline library', async() => {
-      console.log(testDestination);
       //This throws error: ' Command failed with exit code 1 (EPERM) '
-      // await execa('protoc', [GENERATED_CLIENT_LIB_DIR, GOOGLE_GAX_PROTOS_DIR, CLIENT_LIB_DIR, PROTO_DIR], {cwd: testDestination});
-      //test 'echo' command
-      const {stdout} = await execa('protoc', ['--help']);
-      console.log(stdout);
+      await execa('protoc', [GENERATED_CLIENT_LIB_DIR, GOOGLE_GAX_PROTOS_DIR, CLIENT_LIB_DIR, PROTO_DIR]);
+      await execa('cmp', [GENERATED_CLIENT_FILE, CLIENT_LIBRARY_BASELINE]);
     });
   });
 });
