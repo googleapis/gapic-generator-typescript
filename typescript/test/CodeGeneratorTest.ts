7cbd67b4723a4de57329f9f1a01cf2551af1fb15
xiaozhenliugg <xiaozhenliu@google.com>
2019-08-20 12:25:39 -0700

    update path.join for directories

--- typescript/test/CodeGeneratorTest.ts
+++ typescript/test/CodeGeneratorTest.ts
@@ -3,48 +3,40 @@ const util = require('util');
 const path = require('path');
 const fs = require('fs');
 
-const SHOWCASE_CLIENT_LIB = './showcase';
-const TMP_CLIENT_LIB = './showcase/tmp';
+const cwd = process.cwd();
+const SHOWCASE_CLIENT_LIB = path.join(cwd, 'showcase');
+const TMP_CLIENT_LIB = path.join(cwd, 'showcase', 'tmp');
 const GENERATED_CLIENT_LIB_DIR =
-    '--typescript_gapic_out=' + path.join(__dirname, '..', '..') +
-    '/showcase/tmp';
-const GENERATED_CLIENT_FILE = path.join(__dirname, '..', '..') +
-    '/showcase/tmp/src/v1beta1/echo_client.ts';
-const GOOGLE_GAX_PROTOS_DIR = '-I/usr/local/lib/node_modules/google-gax/protos';
+    '--typescript_gapic_out=' + path.join(__dirname, '..', '..', 'showcase', 'tmp');
+const GENERATED_CLIENT_FILE = path.join(__dirname, '..', '..', 'showcase', 'tmp', 'src', 'v1beta1', 'echo_client.ts');
+const GOOGLE_GAX_PROTOS_DIR = '-I/' + path.join('usr', 'local', 'lib', 'node_modules', 'google-gax/protos');
 // TODO: Right now we download protos from release page and cp here. Github
 //  download will give the whole repo.
-const LOCAL_CLIENT_LIB_DIR = '-I/Users/xiaozhenliu/showcase';
-const PROTO_DIR = 'google/showcase/v1beta1/echo.proto';
-const CLIENT_LIBRARY_BASELINE = path.join(__dirname, '..', '..') +
-    '/typescript/test/testdata/echo_client_baseline';
+const LOCAL_CLIENT_LIB_DIR = '-I/' + path.join('Users', 'xiaozhenliu', 'showcase');
+const PROTO_DIR = path.join('google', 'showcase', 'v1beta1', 'echo.proto');
+const CLIENT_LIBRARY_BASELINE = path.join(__dirname, '..', '..', 'typescript', 'test', 'testdata', 'echo_client_baseline');
+
+function compareFiles(file1: string, file2: string): boolean {
+    const clientBuf = fs.readFileSync(file1);
+    const baselineBuf = fs.readFileSync(file2);
+    return clientBuf.equals(baselineBuf);
+}
 
 describe('CodeGeneratorTest', () => {
-  describe('Create ./showcase dir and get the protos', () => {
-    it('Download showcase repo in ./showcase dir.', () => {
-      // Create ./showcase directory.
-      if (!fs.existsSync(SHOWCASE_CLIENT_LIB)) {
-        fs.mkdirSync(SHOWCASE_CLIENT_LIB);
-      }
-    });
-  });
-
-  describe('Create ./showcase/tmp dir', () => {
-    it('./showcase/tmp created', () => {
-      // create ./showcase/tmp/ directory for generated client library
-      if (!fs.existsSync(TMP_CLIENT_LIB)) {
-        fs.mkdirSync(TMP_CLIENT_LIB);
-      }
-    });
-  });
-
   describe('Generate client library', () => {
     it('Generated client library should have same output with baseline.',
        async () => {
+         if (!fs.existsSync(SHOWCASE_CLIENT_LIB)) {
+           fs.mkdirSync(SHOWCASE_CLIENT_LIB);
+         }
+         if (!fs.existsSync(TMP_CLIENT_LIB)) {
+           fs.mkdirSync(TMP_CLIENT_LIB);
+         }
          await execa('protoc', [
            GENERATED_CLIENT_LIB_DIR, GOOGLE_GAX_PROTOS_DIR,
            LOCAL_CLIENT_LIB_DIR, PROTO_DIR
          ]);
-         await execa('cmp', [GENERATED_CLIENT_FILE, CLIENT_LIBRARY_BASELINE]);
+         console.log(compareFiles(GENERATED_CLIENT_FILE, CLIENT_LIBRARY_BASELINE));
        });
   });
 });
