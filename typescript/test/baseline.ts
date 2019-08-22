09f18dc8293f418ec9d038ab7b17938e1cffbd34
Alexander Fenster <fenster@google.com>
2019-08-22 14:21:27 -0700

    make it work

--- typescript/test/baseline.ts
+++ typescript/test/baseline.ts
@@ -1,38 +1,54 @@
-const {execSync} = require('child_process');
-const util = require('util');
-const path = require('path');
-const fs = require('fs');
-const assert = require('assert');
-const cwd = process.cwd();
-const SHOWCASE_CLIENT_LIB = path.join(cwd, 'showcase');
-const TMP_CLIENT_LIB = path.join(cwd, 'showcase', 'tmp');
-const GENERATED_CLIENT_LIB_DIR = '--typescript_gapic_out=' +
-    path.join(__dirname, '..', '..', 'showcase', 'tmp');
-const GENERATED_CLIENT_FILE = path.join(
-    __dirname, '..', '..', 'showcase', 'tmp', 'src', 'v1beta1',
-    'echo_client.ts');
-const GOOGLE_GAX_PROTOS_DIR = '-I/' +
-    path.join('usr', 'local', 'lib', 'node_modules', 'google-gax/protos');
-const LOCAL_CLIENT_LIB_DIR =
-    '-I' + path.join(__dirname, '..', '..', 'typescript', 'test', 'protos');
+import * as assert from 'assert';
+import {execSync} from 'child_process';
+import * as fs from 'fs';
+import {ncp} from 'ncp';
+import * as path from 'path';
+import * as rimraf from 'rimraf';
+import * as util from 'util';
 
-const PROTO_DIR = path.join('google', 'showcase', 'v1beta1', 'echo.proto');
+const cwd = process.cwd();
+const rmrf = util.promisify(rimraf);
+const ncpp = util.promisify(ncp);
+
+const OUTPUT_DIR = path.join(cwd, '.baseline-test-out');
+const GENERATED_CLIENT_FILE =
+    path.join(OUTPUT_DIR, 'src', 'v1beta1', 'echo_client.ts');
+const GOOGLE_GAX_PROTOS_DIR =
+    path.join(cwd, 'node_modules', 'google-gax', 'protos');
+const PROTOS_DIR = path.join(cwd, 'build', 'test', 'protos');
+const ECHO_PROTO_FILE =
+    path.join(PROTOS_DIR, 'google', 'showcase', 'v1beta1', 'echo.proto');
 const CLIENT_LIBRARY_BASELINE = path.join(
-    __dirname, '..', '..', 'typescript', 'test', 'testdata',
-    'echo_client_baseline');
+    cwd, 'typescript', 'test', 'testdata', 'echo_client_baseline.ts.txt');
+const SRCDIR = path.join(cwd, 'build', 'src');
+const CLI = path.join(SRCDIR, 'cli.js');
+const PLUGIN = path.join(SRCDIR, 'protoc-gen-typescript_gapic');
 
 describe('CodeGeneratorTest', () => {
   describe('Generate client library', () => {
     it('Generated client library should have same output with baseline.',
        async () => {
-         if (!fs.existsSync(SHOWCASE_CLIENT_LIB)) {
-           fs.mkdirSync(SHOWCASE_CLIENT_LIB);
+         if (fs.existsSync(OUTPUT_DIR)) {
+           await rmrf(OUTPUT_DIR);
          }
-         if (!fs.existsSync(TMP_CLIENT_LIB)) {
-           fs.mkdirSync(TMP_CLIENT_LIB);
+         fs.mkdirSync(OUTPUT_DIR);
+
+         ncpp(CLI, PLUGIN);
+         process.env['PATH'] = SRCDIR + path.delimiter + process.env['PATH'];
+
+         try {
+           execSync(`chmod +x ${PLUGIN}`);
+         } catch (err) {
+           console.warn(`Failed to chmod +x ${PLUGIN}: ${err}. Ignoring...`);
          }
-         execSync('protoc ' + GENERATED_CLIENT_LIB_DIR + ' ' + GOOGLE_GAX_PROTOS_DIR + ' ' + LOCAL_CLIENT_LIB_DIR + ' ' + PROTO_DIR);
-         assert.strictEqual(fs.readFileSync(GENERATED_CLIENT_FILE), fs.readFileSync(CLIENT_LIBRARY_BASELINE));
+
+         execSync(
+             `protoc --typescript_gapic_out=${OUTPUT_DIR} ` +
+             `-I${GOOGLE_GAX_PROTOS_DIR} ` +
+             `-I${PROTOS_DIR} ` + ECHO_PROTO_FILE);
+         assert.strictEqual(
+             fs.readFileSync(GENERATED_CLIENT_FILE).toString(),
+             fs.readFileSync(CLIENT_LIBRARY_BASELINE).toString());
        });
   });
 });
