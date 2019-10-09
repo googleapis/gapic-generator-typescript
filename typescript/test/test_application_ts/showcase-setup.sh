3f7daadfd951d82133c713d52e481c2a855767e4
xiaozhenliugg <xiaozhenliu@google.com>
2019-10-09 10:39:27 -0700

    fix

--- typescript/test/test_application_ts/showcase-setup.sh
+++ typescript/test/test_application_ts/showcase-setup.sh
@@ -18,6 +18,7 @@ stop_showcase() {
 	# Wait for the process to die, but don't report error from the kill.
 	wait $showcase_pid || true
 }
+tsc --version
 tsc ./src/index.ts
 node ./src/index.js
 if [ $? -eq 0 ]; then
