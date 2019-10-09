c135b15403a394d1810368deaafeb2a511a23204
Xiaozhen Liu <xiaozhenliu@google.com>
2019-10-09 14:16:59 -0700

    test: test application set up (#40)
    * add ts test application
    
    * circleCI test failure, skip ts checking
    
    * baseline test update
    
    * restructure test applications
    
    * path to repo
    
    * fix circleCI test failure
    
    * test root dir for circleCI
    
    * change home path
    
    * npx gts init
    
    * fix
    
    * fix
    
    * tsconfig
    
    * tsconfig
    
    * fix
    
    * update gax version
    
    * feedback
    
    * feedback

---
+++ typescript/test/test_application_ts/src/index.ts
@@ -0,0 +1,106 @@
+import * as assert from 'assert';
+import * as showcase from 'showcase';
+
+import * as grpc from '@grpc/grpc-js'; // to create credentials for local Showcase server
+
+main().catch(err => {
+  console.error('Error:', err);
+});
+
+async function main() {
+const clientOptions = {
+  sslCreds : grpc.credentials.createInsecure(),
+  grpc,
+  servicePath: 'localhost',
+  port: 7469,
+};
+
+  // @ts-ignore TODO: client options types are incompatible
+  const client = new showcase.EchoClient(clientOptions);
+  await testEcho(client);
+  await testExpand(client);
+  await testChat(client);
+  await testCollect(client);
+  console.log('it works! ');
+}
+
+// Set of functions to tests all showcase methods
+async function testEcho(client: showcase.v1beta1.EchoClient) { // TODO: cannot do showcase.EchoClient
+    const request = {
+      content: 'test',
+    };
+    const [response] = await client.echo(request);
+    assert.deepStrictEqual(request.content, response.content);
+}
+
+async function testExpand(client: showcase.v1beta1.EchoClient) {
+    const words = ['nobody', 'ever', 'reads', 'test', 'input'];
+    const request = {
+      content: words.join(' '),
+    };
+    const result = await new Promise((resolve: Function, reject: Function) => {
+      const stream = client.expand(request);
+      const result: string[] = [];
+      stream.on('data', response => {
+        result.push(response.content);
+      });
+      stream.on('end', () => {
+        resolve(result);
+      });
+      stream.on('error', (err) => {
+        reject(err)
+      });
+    });
+    assert.deepStrictEqual(words, result);
+}
+
+async function testChat(client: showcase.v1beta1.EchoClient) {
+  const words = [
+    'nobody',
+    'ever',
+    'reads',
+    'test',
+    'input',
+    'especially',
+    'this',
+    'one',
+  ];
+  const result = await new Promise((resolve: Function, reject: Function) => {
+    const result: string[] = [];
+    const stream = client.chat();
+    stream.on('data', response => {
+      result.push(response.content);
+    });
+    stream.on('end', () => {
+      resolve(result);
+    });
+    stream.on('error', (err) => {
+      reject(err);
+    });
+    for (const word of words) {
+      stream.write({content: word});
+    }
+    stream.end();
+  });
+  assert.deepStrictEqual(result, words);
+}
+
+async function testCollect(client: showcase.v1beta1.EchoClient) {
+  const words = ['nobody', 'ever', 'reads', 'test', 'input'];
+  const result = await new Promise((resolve: Function, reject: Function) => {
+    const stream = client.collect((err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result);
+      });
+    for (const word of words) {
+      const request = {content: word};
+      stream.write(request);
+    }
+    stream.end();
+  });
+  const expectedresponse = {content: words.join(' ')};
+  assert.deepStrictEqual(result, expectedresponse);
+}
