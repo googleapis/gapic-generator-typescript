abf3030e790c76d7e09d8e3fc9e8a2ea8fd528da
xiaozhenliugg <xiaozhenliu@google.com>
2019-11-05 08:29:07 -0800

    feedback

--- docker/test.sh
+++ docker/test.sh
@@ -3,20 +3,21 @@
 ### Test script pulling the docker image and use it against showcase proto.
 
 # Docker image tag: gapic-generator-typescript:latest.
+DIR_NAME=.showcase-typescript
+# Remove test directory if it already exists
+rm -rf $DIR_NAME
 # Create new directory showcase-typescript. 
-mkdir showcase-typescript
-# Pull the docker image
-docker pull gcr.io/gapic-images/gapic-generator-typescript:latest
+mkdir $DIR_NAME
 # Use Docker Image for generating showcase client library
 docker run --rm \
   --mount type=bind,source=`pwd`/typescript/test/protos/google/showcase/v1beta1,destination=/in/typescript/test/protos/google/showcase/v1beta1,readonly \
-  --mount type=bind,source=`pwd`/showcase-typescript,destination=/out \
+  --mount type=bind,source=`pwd`/.showcase-typescript,destination=/out \
   gcr.io/gapic-images/gapic-generator-typescript:latest
 # Test generated client library
-cd showcase-typescript
+cd .showcase-typescript
 npm install  # install dependencies
 npm run fix  # format the code
 npm test     # run unit tests
 
 # Test succeed
-echo 'docker test succedd!'
+echo 'docker test succeeded! '
