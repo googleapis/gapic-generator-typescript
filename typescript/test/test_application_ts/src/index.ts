d52d3668b6e2f78ebcac6cc155a6ee10cdf0bc71
xiaozhenliugg <xiaozhenliu@google.com>
2019-11-05 15:32:03 -0800

    clean

--- typescript/test/test_application_ts/src/index.ts
+++ typescript/test/test_application_ts/src/index.ts
@@ -97,111 +97,117 @@ function runTest(client: showcase.v1beta1.EchoClient, opts: Options) {
 }
 
 // Set of functions to tests all showcase methods
-async function testEcho(client: showcase.v1beta1.EchoClient) {
-  // TODO: cannot do showcase.EchoClient
-  const request = {
-    content: 'test',
-  };
-  const [response] = await client.echo(request);
-  assert.deepStrictEqual(request.content, response.content);
-}
-
-async function testExpand(client: showcase.v1beta1.EchoClient) {
-  const words = ['nobody', 'ever', 'reads', 'test', 'input'];
-  const request = {
-    content: words.join(' '),
-  };
-  const result = await new Promise((resolve: Function, reject: Function) => {
-    const stream = client.expand(request);
-    const result: string[] = [];
-    stream.on('data', response => {
-      result.push(response.content);
-    });
-    stream.on('end', () => {
-      resolve(result);
-    });
-    stream.on('error', err => {
-      reject(err);
-    });
+function testEcho(client: showcase.v1beta1.EchoClient) {
+  it('echo', async () => {
+    const request = {
+      content: 'test',
+    };
+    const [response] = await client.echo(request);
+    assert.deepStrictEqual(request.content, response.content);
   });
-  assert.deepStrictEqual(words, result);
 }
 
-async function testChat(client: showcase.v1beta1.EchoClient) {
-  const words = [
-    'nobody',
-    'ever',
-    'reads',
-    'test',
-    'input',
-    'especially',
-    'this',
-    'one',
-  ];
-  const result = await new Promise((resolve: Function, reject: Function) => {
-    const result: string[] = [];
-    const stream = client.chat();
-    stream.on('data', response => {
-      result.push(response.content);
+function testExpand(client: showcase.v1beta1.EchoClient) {
+  it('expand', async () => {
+    const words = ['nobody', 'ever', 'reads', 'test', 'input'];
+    const request = {
+      content: words.join(' '),
+    };
+    const result = await new Promise((resolve, reject) => {
+      const stream = client.expand(request);
+      const result:string[] = [];
+      stream.on('data', response => {
+        result.push(response.content);
+      });
+      stream.on('end', () => {
+        resolve(result);
+      });
+      stream.on('error', reject);
     });
-    stream.on('end', () => {
-      resolve(result);
-    });
-    stream.on('error', err => {
-      reject(err);
-    });
-    for (const word of words) {
-      stream.write({ content: word });
-    }
-    stream.end();
+    assert.deepStrictEqual(words, result);
   });
-  assert.deepStrictEqual(result, words);
 }
 
-async function testCollect(client: showcase.v1beta1.EchoClient) {
-  const words = ['nobody', 'ever', 'reads', 'test', 'input'];
-  const result = await new Promise((resolve: Function, reject: Function) => {
-    const stream = client.collect((err, result) => {
-      if (err) {
-        reject(err);
-        return;
+function testPagedExpand(client: showcase.v1beta1.EchoClient) {
+  it('pagedExpand', async () => {
+    const words = ['nobody', 'ever', 'reads', 'test', 'input'];
+    const request = {
+      content: words.join(' '),
+      pageSize: 2,
+    };
+    const [response] = await client.pagedExpand(request);
+    const result = response.map(r => r.content);
+    assert.deepStrictEqual(words, result);
+  });
+}
+
+function testCollect(client: showcase.v1beta1.EchoClient) {
+  it('collect', async () => {
+    const words = ['nobody', 'ever', 'reads', 'test', 'input'];
+    const result = await new Promise((resolve, reject) => {
+      const stream = client.collect((err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(result);
+      });
+      for (const word of words) {
+        const request = {content: word};
+        stream.write(request);
       }
-      resolve(result);
+      stream.end();
     });
-    for (const word of words) {
-      const request = { content: word };
-      stream.write(request);
-    }
-    stream.end();
+    assert.deepStrictEqual(result, words.join(' '));
   });
-  const expectedresponse = { content: words.join(' ') };
-  assert.deepStrictEqual(result, expectedresponse);
 }
 
-async function testWait(client: showcase.v1beta1.EchoClient) {
-  const request = {
-    ttl: {
-      seconds: 5,
-      nanos: 0,
-    },
-    success: {
-      content: 'done',
-    },
-  };
-  const [operation, rawResponse] = await client.wait(request);
-  assert(rawResponse!.name !== '');
-  assert(rawResponse!.done === false);
-  const [response] = await operation.promise();
-  assert.deepStrictEqual(response.content, request.success.content);
+function testChat(client: showcase.v1beta1.EchoClient) {
+  it('chat', async () => {
+    const words = [
+      'nobody',
+      'ever',
+      'reads',
+      'test',
+      'input',
+      'especially',
+      'this',
+      'one',
+    ];
+    const result = await new Promise((resolve, reject) => {
+      const result: string[] = [];
+      const stream = client.chat();
+      stream.on('data', response => {
+        result.push(response.content);
+      });
+      stream.on('end', () => {
+        resolve(result);
+      });
+      stream.on('error', reject);
+      for (const word of words) {
+        stream.write({content: word});
+      }
+      stream.end();
+    });
+    assert.deepStrictEqual(result, words);
+  });
 }
 
-async function testPagedExpand(client: showcase.v1beta1.EchoClient) {
-  const words = ['nobody', 'ever', 'reads', 'test', 'input'];
-  const request = {
-    content: words.join(' '),
-    pageSize: 2,
-  };
-  const [response] = await client.pagedExpand(request);
-  const expectedResponse = response.map(r => r.content);
-  assert.deepStrictEqual(expectedResponse, words);
+function testWait(client: showcase.v1beta1.EchoClient) {
+  it('wait', async function() {
+    this.timeout(10000);
+    const request = {
+      ttl: {
+        seconds: 5,
+        nanos: 0,
+      },
+      success: {
+        content: 'done',
+      },
+    };
+    const [operation] = await client.wait(request);
+    const [response] = await operation.promise();
+    assert.deepStrictEqual(response.content, request.success.content);
+  });
 }
+
