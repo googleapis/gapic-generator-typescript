9c211f3fe5f7bba32fc061c9848f1667a5a5c802
xiaozhenliugg <xiaozhenliu@google.com>
2019-11-04 17:23:35 -0800

    typo

--- docker/test.sh
+++ docker/test.sh
@@ -12,7 +12,7 @@ docker run --rm \
   --mount type=bind,source=`pwd`/typescript/test/protos/google/showcase/v1beta1,destination=/in/typescript/test/protos/google/showcase/v1beta1,readonly \
   --mount type=bind,source=`pwd`/showcase-typescript,destination=/out \
   gcr.io/gapic-images/gapic-generator-typescript:latest
-# Test geenrated client library
+# Test generated client library
 cd showcase-typescript
 npm install  # install dependencies
 npm run fix  # format the code
