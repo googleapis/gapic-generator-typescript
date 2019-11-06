642a21d318dc1fe7edd9757837635653136833ae
Xiaozhen Liu <xiaozhenliu@google.com>
2019-11-05 16:20:11 -0800

    chore: add browser-test to test application (#104)
    * browser test set up
    
    * add browser-test for js test application
    
    * setup for ts test application
    
    * browser test work
    
    * clean
    
    * add dom config
    
    * test pass

--- typescript/test/test_application_ts/src/index.ts
+++ typescript/test/test_application_ts/src/index.ts
@@ -17,134 +17,198 @@ import * as showcase from 'showcase';
 
 import * as grpc from '@grpc/grpc-js'; // to create credentials for local Showcase server
 
-main().catch(err => {
-  console.error('Error:', err);
+interface ClientOptions{
+  [name: string] : Object,
+  servicePath: string,
+  port: number
+}
+interface Options{
+  [name: string] : Object
+}
+// Fake auth client for fallback
+const authStub = {
+  getRequestHeaders() {
+    return {Authorization: 'Bearer SOME_TOKEN'};
+  },
+};
+// Finally, using mocha!
+describe('Showcase tests', () => {
+  if (typeof window !== 'undefined') {
+    describe('browser library works', () => {
+      testShowcase({browser: true});
+    });
+  } else {
+    describe('grpc-fallback works', () => {
+      testShowcase({fallback: true});
+    });
+    describe('@grpc/grpc-js works', () => {
+      testShowcase({grpcJs: true});
+    });
+    describe('grpc works', () => {
+      testShowcase({grpc: true});
+    });
+  }
 });
 
-async function main() {
-  const clientOptions = {
-    sslCreds: grpc.credentials.createInsecure(),
-    grpc,
-    servicePath: 'localhost',
-    port: 7469,
-  };
+async function testShowcase(opts: Options) {
+  opts = opts || {};
+  const clientOptions: ClientOptions = {servicePath: 'localhost', port: 7469};
+  let hasStreaming = true;
+  clientOptions.servicePath = 'localhost';
+  clientOptions.port = 7469;
+  if (opts.browser) {
+    clientOptions.protocol = 'http';
+    clientOptions.servicePath = 'localhost';
+    clientOptions.port = 1337;
+    clientOptions.auth = authStub;
+    hasStreaming = false;
+  } else if (opts.fallback) {
+    clientOptions.protocol = 'http';
+    clientOptions.servicePath = 'localhost';
+    clientOptions.port = 1337;
+    clientOptions.auth = authStub;
+    clientOptions.fallback = true;
+    hasStreaming = false;
+  } else if (opts.grpcJs) {
+    const grpc = require('@grpc/grpc-js');
+    clientOptions.sslCreds = grpc.credentials.createInsecure();
+    clientOptions.grpc = grpc;
+  } else if (opts.grpc) {
+    const grpc = require('grpc');
+    clientOptions.sslCreds = grpc.credentials.createInsecure();
+    clientOptions.grpc = grpc;
+  } else {
+    throw new Error('Wrong options passed!');
+  }
+  const client = new showcase.v1beta1.EchoClient(clientOptions);
+  runTest(client, {hasStreaming});
+}
 
-  const client = new showcase.EchoClient(clientOptions);
-  await testEcho(client);
-  await testExpand(client);
-  await testChat(client);
-  await testCollect(client);
-  await testWait(client);
-  await testPagedExpand(client);
-  console.log('it works! ');
+function runTest(client: showcase.v1beta1.EchoClient, opts: Options) {
+  opts = opts || {};
+  testEcho(client);
+  if (opts.hasStreaming) {
+    testExpand(client);
+    testCollect(client);
+    testChat(client);
+  }
+  testPagedExpand(client);
+  testWait(client);
 }
 
 // Set of functions to tests all showcase methods
-async function testEcho(client: showcase.v1beta1.EchoClient) {
-  // TODO: cannot do showcase.EchoClient
-  const request = {
-    content: 'test',
-  };
-  const [response] = await client.echo(request);
-  assert.deepStrictEqual(request.content, response.content);
-}
-
-async function testExpand(client: showcase.v1beta1.EchoClient) {
-  const words = ['nobody', 'ever', 'reads', 'test', 'input'];
-  const request = {
-    content: words.join(' '),
-  };
-  const result = await new Promise((resolve: Function, reject: Function) => {
-    const stream = client.expand(request);
-    const result: string[] = [];
-    stream.on('data', response => {
-      result.push(response.content);
-    });
-    stream.on('end', () => {
-      resolve(result);
-    });
-    stream.on('error', err => {
-      reject(err);
-    });
+function testEcho(client: showcase.v1beta1.EchoClient) {
+  it('echo', async () => {
+    const request = {
+      content: 'test',
+    };
+    const [response] = await client.echo(request);
+    assert.deepStrictEqual(request.content, response.content);
   });
-  assert.deepStrictEqual(words, result);
 }
 
-async function testChat(client: showcase.v1beta1.EchoClient) {
-  const words = [
-    'nobody',
-    'ever',
-    'reads',
-    'test',
-    'input',
-    'especially',
-    'this',
-    'one',
-  ];
-  const result = await new Promise((resolve: Function, reject: Function) => {
-    const result: string[] = [];
-    const stream = client.chat();
-    stream.on('data', response => {
-      result.push(response.content);
+function testExpand(client: showcase.v1beta1.EchoClient) {
+  it('expand', async () => {
+    const words = ['nobody', 'ever', 'reads', 'test', 'input'];
+    const request = {
+      content: words.join(' '),
+    };
+    const result = await new Promise((resolve, reject) => {
+      const stream = client.expand(request);
+      const result:string[] = [];
+      stream.on('data', response => {
+        result.push(response.content);
+      });
+      stream.on('end', () => {
+        resolve(result);
+      });
+      stream.on('error', reject);
     });
-    stream.on('end', () => {
-      resolve(result);
-    });
-    stream.on('error', err => {
-      reject(err);
-    });
-    for (const word of words) {
-      stream.write({ content: word });
-    }
-    stream.end();
+    assert.deepStrictEqual(words, result);
   });
-  assert.deepStrictEqual(result, words);
 }
 
-async function testCollect(client: showcase.v1beta1.EchoClient) {
-  const words = ['nobody', 'ever', 'reads', 'test', 'input'];
-  const result = await new Promise((resolve: Function, reject: Function) => {
-    const stream = client.collect((err, result) => {
-      if (err) {
-        reject(err);
-        return;
+function testPagedExpand(client: showcase.v1beta1.EchoClient) {
+  it('pagedExpand', async () => {
+    const words = ['nobody', 'ever', 'reads', 'test', 'input'];
+    const request = {
+      content: words.join(' '),
+      pageSize: 2,
+    };
+    const [response] = await client.pagedExpand(request);
+    const result = response.map(r => r.content);
+    assert.deepStrictEqual(words, result);
+  });
+}
+
+function testCollect(client: showcase.v1beta1.EchoClient) {
+  it('collect', async () => {
+    const words = ['nobody', 'ever', 'reads', 'test', 'input'];
+    const result = await new Promise((resolve, reject) => {
+      const stream = client.collect((err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(result);
+      });
+      for (const word of words) {
+        const request = {content: word};
+        stream.write(request);
       }
-      resolve(result);
+      stream.end();
     });
-    for (const word of words) {
-      const request = { content: word };
-      stream.write(request);
-    }
-    stream.end();
+    const expectedResult = {content: words.join(' ')};
+    assert.deepStrictEqual(result, expectedResult);
   });
-  const expectedresponse = { content: words.join(' ') };
-  assert.deepStrictEqual(result, expectedresponse);
 }
 
-async function testWait(client: showcase.v1beta1.EchoClient) {
-  const request = {
-    ttl: {
-      seconds: 5,
-      nanos: 0,
-    },
-    success: {
-      content: 'done',
-    },
-  };
-  const [operation, rawResponse] = await client.wait(request);
-  assert(rawResponse!.name !== '');
-  assert(rawResponse!.done === false);
-  const [response] = await operation.promise();
-  assert.deepStrictEqual(response.content, request.success.content);
+function testChat(client: showcase.v1beta1.EchoClient) {
+  it('chat', async () => {
+    const words = [
+      'nobody',
+      'ever',
+      'reads',
+      'test',
+      'input',
+      'especially',
+      'this',
+      'one',
+    ];
+    const result = await new Promise((resolve, reject) => {
+      const result: string[] = [];
+      const stream = client.chat();
+      stream.on('data', response => {
+        result.push(response.content);
+      });
+      stream.on('end', () => {
+        resolve(result);
+      });
+      stream.on('error', reject);
+      for (const word of words) {
+        stream.write({content: word});
+      }
+      stream.end();
+    });
+    assert.deepStrictEqual(result, words);
+  });
 }
 
-async function testPagedExpand(client: showcase.v1beta1.EchoClient) {
-  const words = ['nobody', 'ever', 'reads', 'test', 'input'];
-  const request = {
-    content: words.join(' '),
-    pageSize: 2,
-  };
-  const [response] = await client.pagedExpand(request);
-  const expectedResponse = response.map(r => r.content);
-  assert.deepStrictEqual(expectedResponse, words);
+function testWait(client: showcase.v1beta1.EchoClient) {
+  it('wait', async function() {
+    this.timeout(10000);
+    const request = {
+      ttl: {
+        seconds: 5,
+        nanos: 0,
+      },
+      success: {
+        content: 'done',
+      },
+    };
+    const [operation] = await client.wait(request);
+    const [response] = await operation.promise();
+    assert.deepStrictEqual(response.content, request.success.content);
+  });
 }
+
--- typescript/test/test_application_ts/webpack.config.js
+++ typescript/test/test_application_ts/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
+    entry: './build/src/index.js',
     output: {
       library: "showcaseTest",
       filename: "./main.js"
