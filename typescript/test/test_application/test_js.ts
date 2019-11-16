0ac6451dcd2d56e810c4153530ad273b5928ece8
xiaozhenliugg <xiaozhenliu@google.com>
2019-11-15 17:17:23 -0800

    convert showcase-setup script

--- typescript/test/test_application/test_js.ts
+++ typescript/test/test_application/test_js.ts
@@ -12,9 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { execSync } from 'child_process';
+import * as util from 'util';
+import * as child_process from 'child_process';
 import * as fs from 'fs-extra';
 import * as path from 'path';
+const exec = util.promisify(child_process.exec);
 const SHOWCASE_LIB = path.join(
   __dirname,
   '..',
@@ -51,48 +53,48 @@ const JS_TEST_APPLICATION = path.join(
 );
 describe('TestApplication', () => {
   describe('Test application for js users', () => {
-    it('npm install showcase', function() {
-      this.timeout(10000);
+    it('npm install showcase', async function() {
+      this.timeout(60000);
       // copy protos to generated client library and copy test application to local.
       fs.copySync(PROTOS, path.join(SHOWCASE_LIB, 'protos'));
       fs.copySync(JS_TEST_APPLICATION, LOCAL_JS_APPLICTION);
       process.chdir(SHOWCASE_LIB);
       try {
-        execSync(`npm install`);
+        await exec(`npm install`);
       } catch (err) {
         console.warn(`Failed to install packages.`);
       }
     });
-    it('npm pack showcase library and copy it to test application', function() {
-      this.timeout(10000);
+    it('npm pack showcase library and copy it to test application', async function() {
+      this.timeout(60000);
       try {
-        execSync(`npm pack`);
+       await exec(`npm pack`);
       } catch (err) {
         console.warn(`Failed to pack showcase library`);
       }
       process.chdir(LOCAL_JS_APPLICTION);
       fs.copySync(PACKED_LIB_PATH, path.join(LOCAL_JS_APPLICTION, PACKED_LIB));
     });
-    it('npm install showcase library in test application', function() {
-      this.timeout(10000);
+    it('npm install showcase library in test application', async function() {
+      this.timeout(60000);
       try {
-        execSync(`npm install`);
+        await exec(`npm install`);
       } catch (err) {
         console.warn(`Failed to install showcase library in test application.`);
       }
     });
-    it('run integration in test application', function() {
+    it('run integration in test application', async function() {
       this.timeout(60000);
       try {
-        execSync(`npm test`);
+        await exec(`npm test`);
       } catch (err) {
         console.warn(`Failed to run unit test in test application`);
       }
     });
-    it('run browser test in application', function() {
+    it('run browser test in application', async function() {
       this.timeout(120000);
       try {
-        execSync(`npm run browser-test`);
+        await exec(`npm run browser-test`);
       } catch (err) {
         console.warn(`Failed to run browser test in test application.`);
       }
