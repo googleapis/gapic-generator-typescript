f0509eb8b2efc1807b2ba228311ff39ee44d6134
Xiaozhen Liu <xiaozhenliu@google.com>
2019-11-18 22:58:58 -0800

    chore: convert shell script for ts test application (#143)
    * setup
    
    * convert browser test to js
    
    * convert showcase-setup script
    
    * lint
    
    * feedback
    
    * setup
    
    * format
    
    * test
    
    * fix
    
    * browser test
    
    * make test work
    
    * circle doc
    
    * kill process
    
    * pass test

--- typescript/test/test_application_ts/browser-test.sh
+++
@@ -1,9 +0,0 @@
-showcase-server/gapic-showcase run &
-showcase_pid=$!
-stop_showcase() {
-	kill $showcase_pid
-	# Wait for the process to die, but don't report error from the kill.
-	wait $showcase_pid || true
-}
-karma start
-trap stop_showcase EXIT
