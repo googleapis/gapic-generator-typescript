b3b479b26803f20cb41dc10636ffaa79635277c9
Alexander Fenster <fenster@google.com>
2021-06-17 13:30:11 -0700

    fix typo

--- typescript/test/unit/proto.ts
+++ typescript/test/unit/proto.ts
@@ -324,7 +324,7 @@ describe('src/schema/proto.ts', () => {
         },
         commentsMap: new CommentsMap([fd]),
       });
-      assert.strictEqual(proto.transport, undefined);
+      assert.strictEqual(proto.rest, undefined);
     });
     it('should be true when rest is set', () => {
       const fd = new protos.google.protobuf.FileDescriptorProto();
