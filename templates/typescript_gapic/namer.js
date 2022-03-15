859294dbeb67dcbe856be90d36605e0760b7f7cf
Summer Ji <summerji@google.com>
2022-03-15 00:14:56 -0700

    clean unnesscary id.get

--- templates/typescript_gapic/namer.js
+++ templates/typescript_gapic/namer.js
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const { string } = require("yargs");
-
 /* This helper file must export two functions that will be passed to
  * the template engine.  The purpose of these functions is to perform
  * name conflicts resolution that cannot be handled in the generator
