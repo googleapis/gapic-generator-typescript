33db0b23191fa49d2d4fdce5334234195cfbbd3b
sofisl <55454395+sofisl@users.noreply.github.com>
2024-01-10 12:43:52 -0800

    Update comments.ts

--- typescript/src/schema/comments.ts
+++ typescript/src/schema/comments.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {format} from 'path';
 import type * as protos from '../../../protos/index.js';
 
 // Copied from FieldDescriptorProto to simplify the build
